4f2530fc0217b2fc14376ca489fd55c7fe45ad28
Pallavi TS <pallavi046@gmail.com>
2016-12-15 12:46:28 +0530

    MoneyRush - 	* Removed capitalizing

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,6 @@
 class PlacesController < ApplicationController
   def index
-    data = MoneyRush.pluck(:search_by).uniq.map{ |d| {place_name: d.capitalize} }
+    data = MoneyRush.pluck(:search_by).uniq.map{ |d| {place_name: d} }
     render json: data.to_json
   end
 end
